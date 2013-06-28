af30aa15ca9b0c24fe6f052621357a0fba5fe12e
mschatz <mschatz@4c3258f4-088e-138d-ab3e-e50d4a927f86>
2013-06-28 12:59:03 +0000

    Updated MEX makefile for clarity. Converted MEX files to (hopefully) C89 standard.

--- mex/src/base/flamec/tensor/util/base/TLA_MEX_Convert_primitives.c
+++ mex/src/base/flamec/tensor/util/base/TLA_MEX_Convert_primitives.c
@@ -1,11 +1,12 @@
 #include "TLA_MEX.h"
 
 void TLA_mxa_to_dim_t(const mxArray* mxa, dim_t* val){
+	double* data;
     mexPrintf("mxa class %s\n", mxGetClassName(mxa));
     if(!mxIsDouble(mxa)){
         mexErrMsgTxt("Mode multiplication must be double");
     }
 
-    double* data = mxGetPr(mxa);
+    data = mxGetPr(mxa);
     (*val) = (dim_t)(*data);
 }
