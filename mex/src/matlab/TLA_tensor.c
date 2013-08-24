d96b221374c563175e1cd3c74ff9d1ba321acd40
mschatz <mschatz@4c3258f4-088e-138d-ab3e-e50d4a927f86>
2013-08-24 07:30:10 +0000

    Bulletproofing UNIX Windows tlash mex build scripts

--- mex/src/matlab/TLA_tensor.c
+++ mex/src/matlab/TLA_tensor.c
@@ -2,23 +2,25 @@
 #include "FLAME.h"
 void mexFunction(int nargout, mxArray * pargout[],
                  int nargin, const mxArray * pargin[]){
-    if (nargin > 1)
+	int i;
+	FLA_Obj A;
+
+	if (nargin > 1)
     {
         mexErrMsgTxt("Too many input arguments.");
     }
 
     FLA_Init();
-    int i;
+
 
     //Parse input
-    FLA_Obj A;
-    mexPrintf("Creating blocked tensor\n");
-    TLA_mxa_to_blocked_tensor(pargin[0], &A);
+    mexPrintf("Creating tensor\n");
+    TLA_mxa_to_tensor(pargin[0], &A);
 
     FLA_Obj_print_matlab("A", A);
     //Pass output
-    mexPrintf("Passing A back\n");
-    TLA_blocked_tensor_to_mxa(A, &(pargout[0]));
+    mexPrintf("Passing C back\n");
+    TLA_tensor_to_mxa(A, &(pargout[0]));
 
     //DELETE EVERYTHING
     mexPrintf("finalizing\n");
